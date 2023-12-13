5821449fe580cbb297cfd4bac1eff153fb2c292f
Xuwenjia <49391729+JenniferLiXu@users.noreply.github.com>
2023-12-13 17:23:06 +0100

    probability function

--- Small_Example.R
+++ Small_Example.R
@@ -46,8 +46,9 @@ pcFit <- pc(suffStat = list(C = cor(data), n = nrow(data)),
 # Convert the output to an adjacency matrix
 adjMatrix <- as(pcFit, "amat")
 
+
 n <- 14
-# Create a matrix
+# Create a zero-matrix as our starting betas
 beta_matrix <- matrix(c(0), 
                       nrow = 14, ncol = 14, byrow = TRUE)
 
@@ -57,6 +58,7 @@ diag(beta_matrix) <- NA
 # Convert the matrix to an array
 betas <- array(beta_matrix, dim = c(14, 14))
 
+# Define the starting order
 permy <- c(1:14)
 
 #DAG <-samplescore(n,betas,permy)
@@ -64,13 +66,14 @@ permy <- c(1:14)
 
 
 #Importance Sampling
-importance_beta <- function(beta_values){
+importance_beta <- function(target_beta, beta_values){
   # Initialize the matrix for averaged beta values 
   averaged_beta_matrix <- matrix(0, n, n)  
   # Replace diagonal elements with NA
   diag(averaged_beta_matrix) <- NA
+  weights <- numeric()
+  values <-  numeric()
   
-  values = numeric()
   # Iterate over each matrix position
   for (row in 1:n) {
     for (col in 1:n) {
@@ -103,6 +106,49 @@ DAG_Test <- list()
 DAG_Test[[1]] <- maxEC
 target_beta <- calculateBetaScoresArray(DAG_Test, k = length(DAG_Test), n)
 
+# get the probability of a parent node being the parent of a child node
+get_probability <- function(betas, parentNode, childNode, DAG_Test){  
+  incidence <- DAG_Test[[1]]
+  get_probability <- numeric()
+  if (parentNode != childNode) {
+      # Check if parentNode is actually a parent in this DAG
+    exp_beta <- exp(betas[parentNode,childNode, ])
+    if (incidence[parentNode, childNode] == 1){
+      get_probability <- exp_beta/(1 + exp_beta)
+      }else{
+      get_probability <- 1/(1 + exp_beta)
+      }
+  }else{
+      # weight is not applicable for self (node cannot be its own parent)
+      get_probability <- NA
+  }
+  return(get_probability)
+}
+
+# with weights defined by probabilities 
+is_with_get_weight <- function(beta_values, DAG_Test){
+  # Initialize the matrix for averaged beta values 
+  averaged_beta_matrix <- matrix(0, n, n)  
+  # Replace diagonal elements with NA
+  diag(averaged_beta_matrix) <- NA
+  values <-  numeric()
+  weights <-  numeric()
+  
+  # Iterate over each matrix position
+  for (row in 1:n) {
+    for (col in 1:n) {
+      if (row != col) {  # Exclude diagonal elements
+        values <- beta_values[row, col, ]  # Extract values from the same position in all matrices
+        weights <- get_probability(beta_values, row, col, DAG_Test)  # Define or compute weights for these values
+        
+        # Compute the weighted average and assign it to the averaged matrix
+        averaged_beta_matrix[row, col] <- sum(values * weights)
+      }
+    }
+  }
+  return(averaged_beta_matrix)
+}
+
 prob1<-99
 if(n>3){ prob1<-round(6*99*n/(n^2+10*n-24)) }
 prob1<-prob1/100
@@ -122,10 +168,12 @@ for (round in 1:iter) {
   beta_values <- calculateBetaScoresArray(DAGs, k = length(DAGs) ,n)
   
   # use Importance Sampling to update values of betas
-  betas <- importance_beta(beta_values)
+  betas <- importance_beta(target_beta, beta_values)
   
   # store the weighted beta values
   weighted_betas[[round]] <- betas
+  
+  #weighted_betas[[2]] <- is_with_get_weight(beta_values, DAG_Test)
 }
 
 
